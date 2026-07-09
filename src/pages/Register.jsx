ec25969ab0afd63efc31bedbf91e3567e7c9a1bf
vudlhe182052 <vudlhe182052@fpt.edu.vn>
2026-07-09 23:09:09 +0700

    fix register

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -2,6 +2,8 @@ import { useState } from "react";
 import { registerWithEmail } from "../firebase/auth";
 import { createUserProfile } from "../firebase/firestore";
 import { useNavigate, Link } from "react-router-dom";
+import { useAuth } from "../context/AuthContext";
+
 function Register() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
